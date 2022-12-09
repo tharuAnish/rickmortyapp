943708fb13a12752c02f8a6c5db105f4421a0e15
tharuAnish <namoanishtharu@gmail.com>
2022-12-09 13:10:04 +0530

    state updated - navbar styled

--- src/component/Nav/index.jsx
+++ src/component/Nav/index.jsx
@@ -1,13 +1,18 @@
-import './nav.style.css'
+import "./nav.style.css"
 
-const Nav=()=>{
-    return <div className="nav">
+const Nav = () => {
+  return (
+    <div className="nav">
+      <div className="nav-left">
         <a href="#">Docs</a>
         <a href="#">About</a>
-        <input className='search' type="text" placeholder='Search' />
-         
+        <a target="_blank" href="https://rickandmortyapi.com/api/character">
+          API
+        </a>
+      </div>
+      <input className="search" type="text" placeholder="Search" />
     </div>
+  )
 }
-
 
 export default Nav
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,41 +1,54 @@
-import { useEffect,useState } from 'react'
-import CharacterCard from '../../component/CharacterCard'
-import './home.style.css'
+import { useEffect, useState } from "react"
+import CharacterCard from "../../component/CharacterCard"
+import "./home.style.css"
 
-const Home=()=>{
-//States
-const [characterData,setCharacterData]=useState([])
-const [isLoading,setIsLoading]=useState(false)
+const Home = () => {
+  //States
+  const [characterData, setCharacterData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
+  // const fetchCharacterr=async ()=>{
+  //     const json=await fetch("https://rickandmortyapi.com/api/character");
+  //     const data=await json.json()
+  //     setCharacterData(data.results)
+  // }
 
-const fetchCharacterr=async ()=>{
-    const json=await fetch("https://rickandmortyapi.com/api/character");
-    const data=await json.json()
-    setCharacterData(data.results)
-}
+  const fetchCharacter = async () => {
+    setIsLoading(true)
+    try {
+      const res = await fetch("https://rickandmortyapi.com/api/character")
+      const data = await res.json()
+      setCharacterData(data.results)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+    }
+  }
 
-//    Sideeffect
-useEffect(()=>{
-   fetchCharacterr()
-},[])
+  //    Sideeffect
+  useEffect(() => {
+    fetchCharacter()
+  }, [])
 
+  return (
+    <div className="wrapper">
+      <div className="background">
+        {/* <img src="../../../img/rickmorty-wallpaper.webp" alt="" /> */}
+        <h1>RickAndMortyApp</h1>
+      </div>
 
-    return <div className="wrapper">
-        <div className="background">
-            {/* <img src="../../../img/rickmorty-wallpaper.webp" alt="" /> */}
-            <h1>RickAndMortyApp</h1>
-        </div>
-        
-        <div className="characterWrapper">
-        { characterData.length ? characterData.map((item,i)=>
-            <CharacterCard key={i} data={item} isLoading={isLoading}/>
-            )  : 
-            <h1>Loading..</h1>
-            }
+      <div className="characterWrapper">
+        {characterData.length ? (
+          characterData.map((item, i) => (
+            <CharacterCard key={i} data={item} isLoading={isLoading} />
+          ))
+        ) : (
+          <h1>Loading..</h1>
+        )}
+      </div>
     </div>
-         
-    </div>
+  )
 }
-
 
 export default Home
